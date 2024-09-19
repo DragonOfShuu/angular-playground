8fca94d23b6ba7f899ee47227fe6a7d85f503633
Logan Cederlof <68718280+DragonOfShuu@users.noreply.github.com>
2024-09-19 01:15:01 -0600

    Hideable Image

--- src/app/puck-image/puck-image.component.ts
+++ src/app/puck-image/puck-image.component.ts
@@ -6,11 +6,20 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [NgOptimizedImage],
   template: `
-    <img ngSrc="/puck-image.png" alt="Puck from the anime re:zero" width="512" height="512" />
+    <button (click)="buttonToggle()" class="button">
+      Hide Image
+    </button>
+    @if (imageVisible) {
+      <img ngSrc="/puck-image.png" alt="Puck from the anime re:zero" width="512" height="512" />
+    }
   `,
   styles: ``
 })
 
 export class PuckImageComponent {
+  imageVisible: boolean = true;
 
+  buttonToggle() {
+    this.imageVisible = !this.imageVisible;
+  }
 }
